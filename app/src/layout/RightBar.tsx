b48ac2d5e97b9cfb2f993b5493653cab9ae14c09
Dechanuphap Anuwan <dechanuphap.a@kkumail.com>
2023-09-13 02:21:04 +0700

    Create RightBar component (Split from ChatInterface component)

---
+++ app/src/layout/RightBar.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+export default function RightBar() {
+    return (
+        <div
+            className={`grow-0 shrink-0 h-screen border-l min-h-0 transition-all w-80`}
+        >
+
+
+        </div>
+    );
+}
