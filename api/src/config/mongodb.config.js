171ced34cf8d67fd462fe4990727c31c48f94b0e
VillSource <anirut1800@gmail.com>
2023-09-12 18:55:56 +0700

    conf: ➕ add mongodb connection

---
+++ api/src/config/mongodb.config.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const databaseHost = process.env.MONGO_DB_HOST || 'database';
+const databasePort = process.env.MONGO_DB_PORT || '27017';
+const databaseName = process.env.MONGO_DB_DATABASE || 'chat';
+
+const connectDatabase = async () =>{
+    await mongoose.connect(`mongodb://${databaseHost}:${databasePort}/${databaseName}`).then(()=>{
+        console.info("Connected to database " + databaseName);
+    }).catch(err => console.error);
+}
+
+module.exports = connectDatabase;
