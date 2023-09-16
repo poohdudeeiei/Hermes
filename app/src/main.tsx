3ac610e99d85513a7837a182bdb6f59e6499daa0
Puppydude <suphasan.s@kkumail.com>
2023-09-16 23:52:40 +0700

    Update AuthContext waitng for API and Create form validation for login page

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// import AuthContext from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        {/* <AuthContext> */}
+            <App />
+        {/* </AuthContext> */}
+    </React.StrictMode>,
 )
--- app/src/pages/LoginPage.tsx
+++ app/src/pages/LoginPage.tsx
@@ -1,17 +1,102 @@
+import * as yup from "yup";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import Brand from "../layout/Sidebar/Brand/Brand";
+
 function LoginPage() {
+
+    type Person = {
+        username: string;
+        password: string;
+    };
+
+    const schema: yup.ObjectSchema<Person> = yup.object().shape({
+        username: yup.string().required(),
+        password: yup.string().required(),
+    });
+
+    const defaultValues: Person = {
+        username: "",
+        password: "",
+    };
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Person>({ defaultValues, mode: "onBlur", resolver: yupResolver(schema) });
+
+    // const auth = useAuth()
+
+    const onSubmit = (data: Person) => {
+        console.log(data);
+        // const { username, password } = data
+        // auth.login({ email, password}, () => {
+        //   setError('email', {
+        //     type: 'manual',
+        //     message: 'Email or Password is invalid'
+        //   })
+        // })
+    };
+
     return (
-        <div className="h-screen flex justify-center items-center bg-gray-50">
-            <div className="bg-white p-6 rounded-lg shadow">
-                <div className="mb-4">
-                    <input type="text" className="px-4 py-2 rounded-lg border" placeholder="Username"></input>
-                </div>
-                <div className="mb-4">
-                    <input type="password" className="px-4 py-2 rounded-lg border" placeholder="Password"></input>
-                </div>
-                <div className="mb-6 relative">
-                    <button type="button" className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white font-semibold">Login</button>
-                </div>
-                <button className="tex-sm">Sign up</button>
+        <div  className="h-screen justify-center items-center flex flex-col">
+            <Brand />
+            <div className="h-fit flex justify-center items-center bg-gray-50">
+
+                <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="bg-white w-fit p-6 rounded-lg shadow ">
+                        <div className="mb-4">
+                            <Controller
+                                name="username"
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field: { value, onChange, onBlur } }) => (
+                                    <input
+                                        className="px-4 py-2 rounded-lg border"
+                                        id="login-username"
+                                        value={value}
+                                        onBlur={onBlur}
+                                        onChange={onChange}
+                                        placeholder="Username"
+                                    />
+                                )}
+                            />
+                            {errors.username && (
+                                <p className="text-red-500 text-sm">{errors.username.message}</p>
+                            )}
+
+                            {/* <input type="text" className="px-4 py-2 rounded-lg border" placeholder="Username"></input> */}
+                        </div>
+                        <div className="mb-4">
+
+                            <Controller
+                                name="password"
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field: { value, onChange, onBlur } }) => (
+                                    <input
+                                        className="px-4 py-2 rounded-lg border"
+                                        id="login-password"
+                                        type="password"
+                                        value={value}
+                                        onBlur={onBlur}
+                                        onChange={onChange}
+                                        placeholder="Password"
+                                    />
+                                )}
+                            />
+                            {errors.password && (
+                                <p className="text-red-500 text-sm">{errors.password.message}</p>
+                            )}
+                            {/* <input type="password" className="px-4 py-2 rounded-lg border" placeholder="Password"></input> */}
+                        </div>
+                        <div className="mt-10 my-6 relative">
+                            <button type="submit" className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white font-semibold">Login</button>
+                        </div>
+                        {/* <button className="tex-sm">Sign up</button> */}
+                    </div>
+                </form>
             </div>
         </div>
     );
